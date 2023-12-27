b66920d3d5fd4cbc7305f7a068bc6f5a7f17e331
akash dudhal <akashdudhal62@gmail.com>
2023-12-27 21:22:39 +0530

    login form component create

---
+++ src/app/Login-Form/login-Form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:"app-login-form",
+    templateUrl:"./login-Form.component.html",
+    styleUrls:["./login-Form.component.css"]
+})
+export class LoginFormComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginFormComponent } from './Login-Form/login-Form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
