899085bbe036dc0a0c3e4f05d7b05602c5f1a9ef
akash dudhal <akashdudhal62@gmail.com>
2023-12-27 22:55:49 +0530

    create normal form and store a takan to storage

--- src/app/Login-Form/login-Form.component.ts
+++ src/app/Login-Form/login-Form.component.ts
@@ -1,10 +1,23 @@
 import { Component } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
+
 
 @Component({
-    selector:"app-login-form",
-    templateUrl:"./login-Form.component.html",
-    styleUrls:["./login-Form.component.css"]
+    selector: "app-login-form",
+    templateUrl: "./login-Form.component.html",
+    styleUrls: ["./login-Form.component.css"]
 })
 export class LoginFormComponent {
+
+    loginForm: FormGroup = new FormGroup({
+        'username': new FormControl(null),
+        'password': new FormControl(null)
+    });
+
+    logedIn() {
+        console.log(this.loginForm.value);
+        sessionStorage.setItem('token','abcd@123123');
+    }
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginFormComponent } from './Login-Form/login-Form.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from "@angular/material/button"
 
 const appRoute: Routes = [
   { path: "", component: LoginFormComponent }
@@ -17,6 +20,9 @@ const appRoute: Routes = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
     RouterModule.forRoot(appRoute)
   ],
   providers: [],
